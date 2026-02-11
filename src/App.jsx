b83efbb60411faeafd3789aa9a07c1b3c6c50af2
Umesh Chandra Gopalasetti <umeshchandra.g01@gmail.com>
2026-02-11 23:03:36 +0530

    vercel added and / -> chats render

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
       <Toaster position="top-right" reverseOrder={false} />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Chats />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/chats" element={<Chats />} />
         </Routes>
       </BrowserRouter>
     </>
